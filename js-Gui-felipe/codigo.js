6155b23f2ef7df143ca11ede8889b602f7aac5cd
EstouradordeGola <161615704+EstouradordeGola@users.noreply.github.com>
2024-08-06 11:40:36 -0300

    dei erro resultado

---
+++ js-Gui-felipe/codigo.js
@@ -0,0 +1,15 @@
+//declaração de variáveis
+
+let quantidade = 0;
+let total = 0;
+
+function calculafelipe(){
+//promp para entrada de dados
+
+quantidade = parseInt(prompt("Digite o numero de vezes que o Felipe nos deixou sem oxigenio "));
+
+
+total = total + quantidade;
+
+alert(total);
+}
